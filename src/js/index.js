a54a58d3319107e219414b34d95b14fa94d361f0
CzajnaExe <suberlakm@gmail.com>
2020-07-06 21:14:47 +0200

    style and git repository to look as a console

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('HELLO 🚀')
+const GIT_API_URL_REPOS = 'https://api.github.com/users/mateuszsuberlak/repos?sort=created&direction=desc';
+
+const githubRepoListElement = document.querySelector('.github-repo-list--js');
+
+fetch(GIT_API_URL_REPOS)
+  .then(response => response.json())
+  .then(data => data.map(x => SetGitHubReposiotryData(x)))
+  .catch((error) => {
+    console.error("Error accessing github api:", error);
+  });
+
+
+  function SetGitHubReposiotryData(repoData){
+
+  }
