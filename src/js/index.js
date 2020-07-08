4c644238dc89f2901a0ebcf71a1b70e4a0757246
CzajnaExe <suberlakm@gmail.com>
2020-07-08 23:10:12 +0200

    JS script to get repose and add the to dom

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ const GIT_API_URL_REPOS = 'https://api.github.com/users/mateuszsuberlak/repos?so
 
 const githubRepoListElement = document.querySelector('.github-repo-list--js');
 
+const githubConsoleTemplate = document.querySelector('.github-repo-console-template--js')
+
 fetch(GIT_API_URL_REPOS)
   .then(response => response.json())
   .then(data => data.map(x => SetGitHubReposiotryData(x)))
@@ -13,5 +15,22 @@ fetch(GIT_API_URL_REPOS)
 
 
   function SetGitHubReposiotryData(repoData){
+    console.log('repoData-',repoData);
+    const consoleTemplateClone = githubConsoleTemplate.content.cloneNode(true);
+
+    const repoNameElement = consoleTemplateClone.querySelector('.github-project-name--js');
+    repoNameElement.textContent = repoData.name
+
+    const repoDescriptionElement = consoleTemplateClone.querySelector('.github-project-description--js');
+    repoDescriptionElement.textContent = repoData.description
+
+    const repoDemoUrlElement = consoleTemplateClone.querySelector('.github-project-demo-url--js');
+    console.log(repoDemoUrlElement);
+    repoDemoUrlElement.innerHTML =  repoData.homepage ? '<a href="' +repoData.homepage+'">see here</a>' : 'none';
+
+    const repoUrlElement = consoleTemplateClone.querySelector('.github-project-repo-url--js');
+    repoUrlElement.innerHTML = repoData.html_url ? '<a href="' +repoData.html_url+'">source_code</a>' : 'none';
+
+    githubRepoListElement.appendChild(consoleTemplateClone);
 
   }
