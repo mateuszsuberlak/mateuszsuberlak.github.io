84168d259570ebe07cd9aa4a6a383f1b821b1772
CzajnaExe <suberlakm@gmail.com>
2020-07-26 15:42:21 +0200

    Fixes for links

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,60 @@
 import "../scss/main.scss";
 
-const GIT_API_URL_REPOS = 'https://api.github.com/users/mateuszsuberlak/repos?sort=created&direction=desc';
+const GIT_API_URL_REPOS =
+  "https://api.github.com/users/mateuszsuberlak/repos?sort=created&direction=desc";
 
-const githubRepoListElement = document.querySelector('.github-repo-list--js');
+const githubRepoListElement = document.querySelector(".github-repo-list--js");
 
-const githubConsoleTemplate = document.querySelector('.github-repo-console-template--js')
+const githubConsoleTemplate = document.querySelector(
+  ".github-repo-console-template--js"
+);
 
 fetch(GIT_API_URL_REPOS)
-  .then(response => response.json())
-  .then(data => data.map(x => SetGitHubReposiotryData(x)))
+  .then((response) => response.json())
+  .then((data) => data.map((x) => SetGitHubReposiotryData(x)))
   .catch((error) => {
     console.error("Error accessing github api:", error);
   });
 
+function SetGitHubReposiotryData(repoData) {
+  const consoleTemplateClone = githubConsoleTemplate.content.cloneNode(true);
 
-  function SetGitHubReposiotryData(repoData){
-    console.log('repoData-',repoData);
-    const consoleTemplateClone = githubConsoleTemplate.content.cloneNode(true);
+  const repoNameElement = consoleTemplateClone.querySelector(
+    ".github-project-name--js"
+  );
+  repoNameElement.textContent = repoData.name;
 
-    const repoNameElement = consoleTemplateClone.querySelector('.github-project-name--js');
-    repoNameElement.textContent = repoData.name
+  const repoDescriptionElement = consoleTemplateClone.querySelector(
+    ".github-project-description--js"
+  );
+  repoDescriptionElement.textContent = repoData.description;
 
-    const repoDescriptionElement = consoleTemplateClone.querySelector('.github-project-description--js');
-    repoDescriptionElement.textContent = repoData.description
-
-    const repoDemoUrlElement = consoleTemplateClone.querySelector('.github-project-demo-url--js');
-    console.log(repoDemoUrlElement);
-    repoDemoUrlElement.innerHTML =  repoData.homepage ? '<a href="' +repoData.homepage+'">see here</a>' : 'none';
-
-    const repoUrlElement = consoleTemplateClone.querySelector('.github-project-repo-url--js');
-    repoUrlElement.innerHTML = repoData.html_url ? '<a href="' +repoData.html_url+'">source_code</a>' : 'none';
-
-    githubRepoListElement.appendChild(consoleTemplateClone);
-
+  const repoDemoUrlElement = consoleTemplateClone.querySelector(
+    ".github-project-demo-url--js"
+  );
+  console.log(repoDemoUrlElement);
+  if (repoData.homepage) {
+    repoDemoUrlElement.href = repoData.homepage;
+    repoDemoUrlElement.title = "Demo: " + repoData.name + " see here";
+    repoDemoUrlElement.innerHTML = "see here";
+  } else {
+    repoDemoUrlElement.removeAttribute("href");
+    repoDemoUrlElement.parentElement.className += " github-repo-console-content--line-right__link--is-Disabled";
   }
+
+  const repoUrlElement = consoleTemplateClone.querySelector(
+    ".github-project-repo-url--js"
+  );
+
+  if (repoData.html_url) {
+    repoUrlElement.href = repoData.html_url;
+    repoUrlElement.title = "Github repository:" + repoData.name + " source code";
+    repoUrlElement.innerHTML = "source code";
+  }
+  else {
+    repoDemoUrlElement.removeAttribute("href");
+    repoDemoUrlElement.parentElement.className += " github-repo-console-content--line-right__link--is-Disabled";
+  }
+
+  githubRepoListElement.appendChild(consoleTemplateClone);
+}
